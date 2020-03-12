ed8c1931ba76750dee91bf42658dcbf4e869f09a
Dylan Rankin <dylansr90@gmail.com>
2020-03-12 19:49:51 +0000

    updating for fpga

--- Makefile
+++ Makefile
@@ -27,12 +27,19 @@ LDFLAGS = -Wl,-rpath,/usr/lib \
  $(CURDIR)/../../cmake/build/third_party/abseil-cpp/absl/types/libabsl_bad_variant_access.a\
  $(CURDIR)/../../cmake/build/third_party/abseil-cpp/absl/numeric/libabsl_int128.a\
  -lnsl $(CURDIR)/../../cmake/build/libaddress_sorting.a\
+ $(opencl_LDFLAGS)\
  -ldl -lrt -lm -lssl
 #/usr/local/lib/libgrpc++_unsecure.a /usr/local/lib64/libprotobuf.a -lpthread /usr/local/lib/libgrpc_unsecure.a /usr/local/lib/libgpr.a /usr/local/lib/libz.so /usr/local/lib/libcares.so.2.3.0 -lnsl /usr/local/lib/libaddress_sorting.a -ldl -lrt -lm 
 
+COMMON_REPO := ./hls4ml_c/
+#include $(COMMON_REPO)/utility/utils.mk
+include $(COMMON_REPO)/libs/xcl2/xcl2.mk
+include $(COMMON_REPO)/libs/opencl/opencl.mk
+
+HLS4ML_PROJ_TYPE := DENSE
 
 CXX = g++
-CPPFLAGS += `pkg-config --cflags protobuf grpc` -I$(CURDIR)/../../include
+CPPFLAGS += `pkg-config --cflags protobuf grpc` -I$(CURDIR)/../../include -I$(COMMON_REPO)/src/ -I$(COMMON_REPO)/src/nnet_utils/ $(xcl2_CXXFLAGS) $(opencl_CXXFLAGS) -DIS_$(HLS4ML_PROJ_TYPE) -DWEIGHTS_DIR=$(COMMON_REPO)/src/weights/ -I$(XILINX_VIVADO)/include/ -I$(XILINX_VITIS)/include/ -Wno-unknown-pragmas -I$(XILINX_XRT)/include/
 CXXFLAGS += -std=c++11
 
 GRPC_CPP_PLUGIN = grpc_cpp_plugin
@@ -40,10 +47,13 @@ GRPC_CPP_PLUGIN_PATH ?= `which $(GRPC_CPP_PLUGIN)`
 
 all: server client
 
+xcl2.o: $(xcl2_SRCS) $(xcl2_HDRS)
+	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -c -o $@ $<
+
 client: api.pb.o model_config.pb.o request_status.pb.o server_status.pb.o grpc_service.pb.o api.grpc.pb.o model_config.grpc.pb.o request_status.grpc.pb.o server_status.grpc.pb.o grpc_service.grpc.pb.o client.o
 	$(CXX) $^ $(LDFLAGS) -o $@
 
-server: api.pb.o model_config.pb.o request_status.pb.o server_status.pb.o grpc_service.pb.o api.grpc.pb.o model_config.grpc.pb.o request_status.grpc.pb.o server_status.grpc.pb.o grpc_service.grpc.pb.o server.o
+server: api.pb.o model_config.pb.o request_status.pb.o server_status.pb.o grpc_service.pb.o api.grpc.pb.o model_config.grpc.pb.o request_status.grpc.pb.o server_status.grpc.pb.o grpc_service.grpc.pb.o server.o xcl2.o
 	$(CXX) $^ $(LDFLAGS) -o $@
 
 %.grpc.pb.cc: %.proto
--- server.cc
+++ server.cc
@@ -2,10 +2,21 @@
 #include <memory>
 #include <string>
 
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
+
+#include "xcl2.hpp"
+#include <vector>
+
 #include <grpcpp/grpcpp.h>
 #include "grpc_service.grpc.pb.h"
 #include "request_status.grpc.pb.h"
 #include "model_config.pb.h"
+
+#include "kernel_params.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -22,6 +33,17 @@ using nvidia::inferenceserver::DataType;
 
 class GRPCServiceImplementation final : public nvidia::inferenceserver::GRPCService::Service {
 
+ public:
+  std::vector<data_t,aligned_allocator<data_t>> source_in;
+  std::vector<data_t,aligned_allocator<data_t>> source_hw_results;
+//(DATA_SIZE_IN*STREAMSIZE);
+//(DATA_SIZE_OUT*STREAMSIZE);
+  std::vector<cl::Memory> inBufVec, outBufVec;
+  cl::Kernel krnl_xil;
+  cl::Program program;
+  cl::CommandQueue q;
+
+ private: 
   grpc::Status Status(
 		     ServerContext* context, 
 		     const StatusRequest* request, 
@@ -38,7 +60,7 @@ class GRPCServiceImplementation final : public nvidia::inferenceserver::GRPCServ
     auto input = config->add_input();
     input->set_name("input");
     input->set_data_type(DataType::TYPE_FP32);
-    input->add_dims(15);
+    input->add_dims(18);
     auto output = config->add_output();
     output->set_name("output/BiasAdd");
     output->set_data_type(DataType::TYPE_FP32);
@@ -59,7 +81,7 @@ class GRPCServiceImplementation final : public nvidia::inferenceserver::GRPCServ
     const void* lVals = raw.c_str();
     float* lFVals = (float*) lVals;
     //output array that is equal to ninputs(15)*batch flot is 4 bits
-    unsigned batch_size = raw.size()/15/4;
+    unsigned batch_size = raw.size()/18/4;
     reply->mutable_request_status()->set_code(RequestStatusCode::SUCCESS);
     reply->mutable_request_status()->set_server_id("inference:0");
     reply->mutable_meta_data()->set_id(request->meta_data().id());
@@ -71,26 +93,39 @@ class GRPCServiceImplementation final : public nvidia::inferenceserver::GRPCServ
     output1->set_name("output/BiasAdd");
     output1->mutable_raw()->mutable_dims()->Add(1);
     output1->mutable_raw()->set_batch_byte_size(8*batch_size);
+
+    std::copy(lFVals,lFVals+batch_size*DATA_SIZE_IN,source_in.begin());
+
+    // Copy input data to device global memory
+    q.enqueueMigrateMemObjects(inBufVec,0/* 0 means from host*/);
+    // Launch the Kernel
+    // For HLS kernels global and local size is always (1,1,1). So, it is recommended
+    // to always use enqueueTask() for invoking HLS kernel
+    q.enqueueTask(krnl_xil);
+    // Copy Result from Device Global Memory to Host Local Memory
+    q.enqueueMigrateMemObjects(outBufVec,CL_MIGRATE_MEM_OBJECT_HOST);
+    // Check for any errors from the command queue
+    q.finish();
     
-    //Fnally deal with the ouputs
-    float* lTVals = new float[1];
-    for(int i0 = 0; i0 < 1; i0++) lTVals[i0] = 6.;
-    char* tmp = (char*) lTVals;
     std::string *outputs1 = reply->add_raw_output();
-    for(unsigned i0 = 0; i0 < batch_size; i0++) { 
+    float* lTVals = new float[batch_size];
+    std::copy(source_hw_results.begin(), source_hw_results.begin()+batch_size*DATA_SIZE_OUT, lTVals);
+    for(unsigned i0 = 0; i0 < batch_size; i0++) {
+      char* tmp = (char*) (lTVals+i0);
       outputs1->append(tmp,sizeof(tmp));
     }
+
     return grpc::Status::OK;
   } 
 };
 
-void Run() {
+void Run(std::string xclbinFilename) {
   std::string address("0.0.0.0:8443");
   GRPCServiceImplementation service;
 
   ServerBuilder builder;
   builder.AddListeningPort(address, grpc::InsecureServerCredentials());
-  builder.SetMaxMessageSize(640000);
+  builder.SetMaxMessageSize(1000000);
   builder.RegisterService(&service);
   //All the crap that trt inference server runs
   //std::unique_ptr<grpc::ServerCompletionQueue> health_cq = builder.AddCompletionQueue();
@@ -100,6 +135,70 @@ void Run() {
   //std::unique_ptr<grpc::ServerCompletionQueue> stream_infer_cq = builder.AddCompletionQueue();
   //std::unique_ptr<grpc::ServerCompletionQueue> modelcontrol_cq = builder.AddCompletionQueue();
   //std::unique_ptr<grpc::ServerCompletionQueue> shmcontrol_cq   = builder.AddCompletionQueue();
+
+  size_t vector_size_in_bytes = sizeof(data_t) * DATA_SIZE_IN * STREAMSIZE;
+  size_t vector_size_out_bytes = sizeof(data_t) * DATA_SIZE_OUT * STREAMSIZE;
+  // Allocate Memory in Host Memory
+  // When creating a buffer with user pointer (CL_MEM_USE_HOST_PTR), under the hood user ptr 
+  // is used if it is properly aligned. when not aligned, runtime had no choice but to create
+  // its own host side buffer. So it is recommended to use this allocator if user wish to
+  // create buffer using CL_MEM_USE_HOST_PTR to align user buffer to page boundary. It will 
+  // ensure that user buffer is used when user create Buffer/Mem object with CL_MEM_USE_HOST_PTR 
+
+  //initialize
+  for(int j = 0 ; j < DATA_SIZE_IN*STREAMSIZE ; j++){
+      service.source_in.push_back((data_t)0);
+  }
+  for(int j = 0 ; j < DATA_SIZE_OUT*STREAMSIZE ; j++){
+      service.source_hw_results.push_back((data_t)0);
+  }
+
+  std::vector<cl::Device> devices = xcl::get_xil_devices();
+  cl::Device device = devices[0];
+  devices.resize(1);
+
+  cl::Context context(device);
+  cl::CommandQueue q_tmp(context, device, CL_QUEUE_PROFILING_ENABLE);
+  service.q = q_tmp;
+  std::string device_name = device.getInfo<CL_DEVICE_NAME>();
+  std::cout << "Found Device=" << device_name.c_str() << std::endl;
+
+  cl::Program::Binaries bins;
+  // Load xclbin
+  std::cout << "Loading: '" << xclbinFilename << "'\n";
+  std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
+  bin_file.seekg (0, bin_file.end);
+  unsigned nb = bin_file.tellg();
+  bin_file.seekg (0, bin_file.beg);
+  char *buf = new char [nb];
+  bin_file.read(buf, nb);
+
+  // Creating Program from Binary File
+  bins.push_back({buf,nb});
+
+  cl::Program tmp_program(context, devices, bins);
+  service.program = tmp_program;
+
+  // Allocate Buffer in Global Memory
+  // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and 
+  // Device-to-host communication
+  cl::Buffer buffer_in   (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+          vector_size_in_bytes, service.source_in.data());
+  cl::Buffer buffer_output(context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+          vector_size_out_bytes, service.source_hw_results.data());
+
+  service.inBufVec.clear();
+  service.outBufVec.clear();
+  service.inBufVec.push_back(buffer_in);
+  service.outBufVec.push_back(buffer_output);
+
+  cl::Kernel krnl_aws_hls4ml(service.program,"aws_hls4ml");
+  service.krnl_xil = krnl_aws_hls4ml;
+
+  int narg = 0;
+  service.krnl_xil.setArg(narg++, buffer_in);
+  service.krnl_xil.setArg(narg++, buffer_output);
+
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on port: " << address << std::endl;
   int server_id=1;
@@ -107,7 +206,10 @@ void Run() {
 }
 
 int main(int argc, char** argv) {
-  Run();
+
+  std::string xclbinFilename = "";
+  if (argc>1) xclbinFilename = argv[1];
+  Run(xclbinFilename);
 
   return 0;
 }
